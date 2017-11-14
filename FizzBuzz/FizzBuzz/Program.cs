3edcf64a639868a02e53c844cd717bb9a249cfb1
Amy Funk <amy61789@outlook.com>
2017-11-14 08:43:16 -0800

    Created a method that takes a number as an argument. For multiples of three return “Fizz” For multiples of five return “Buzz”. For numbers which are multiples of both three and five return “FizzBuzz”

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -10,6 +10,31 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i < 51; i++)
+            {
+                FizzBuzz(i);
+            }
+
+            Console.Read();
         }
+        public static void FizzBuzz(int number)
+        {
+            if (number % 15 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }else if(number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }else if(number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+
     }
 }
